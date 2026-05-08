fe9ec5aaf1ca85f5283034e9d775a7994f87d17e
natibuckeye <davisdocs17@gmail.com>
2026-05-07 22:31:55 -0400

    packing.js

--- modules/packing.js
+++ modules/packing.js
@@ -1,1 +1,9 @@
+export function loadPacking() {
+  document.getElementById("app").innerHTML = `
+    <h2 class="module-title">Packing List</h2>
+    <div class="module-card">
+      <p>Your personalized packing list will appear here.</p>
+    </div>
+  `;
+}
 
